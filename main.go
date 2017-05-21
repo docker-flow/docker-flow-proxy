08194b7def2d7980bc04b86d960026d266efd980
Viktor <viktor@farcic.com>
2017-05-21 16:40:40 +0200

    Changed GoLang default log output from stderr to stdout [fixes #234]

--- main.go
+++ main.go
@@ -4,9 +4,11 @@ import (
 	"./logging"
 	"os"
 	"strings"
+	"log"
 )
 
 func main() {
+	log.SetOutput(os.Stdout)
 	if strings.EqualFold(os.Getenv("DEBUG"), "true") {
 		go logging.StartLogging()
 	}
